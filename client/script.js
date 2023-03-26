810d7061eea3a479c03462ce09c5d04778d9e4e2
Chaves <contactrenchaves@gmail.com>
2023-03-26 17:14:50 -0300

    update the server url

--- client/script.js
+++ client/script.js
@@ -86,6 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
+    
     const response = await fetch('https://chavesdevopenai.onrender.com', {
         method: 'POST',
         headers: {
@@ -94,7 +95,7 @@ const handleSubmit = async (e) => {
         body: JSON.stringify({
             prompt: data.get('prompt')
         })
-    })
+    })//comentario
 
     clearInterval(loadInterval)
     messageDiv.innerHTML = " "
