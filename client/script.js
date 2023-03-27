4431e1e360482ed06012daa5e11e8f26aeb9da53
Chaves <contactrenchaves@gmail.com>
2023-03-27 14:43:08 -0300

    updated the server url

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv)
 
 
-    const response = await fetch("https://chavesdevopenai.onrender.com", {
+    const response = await fetch('https://chavesdevopenai.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
